21e8c869724b81ed0f9593058074839b1db5d33d
Matthew Hammond <matthew.ian.hammond@gmail.com>
2019-08-08 09:23:00 +0100

    Removed tests that aren't complete and ause errors

--- IndexTestFunctions.php
+++ IndexTestFunctions.php
@@ -1,14 +1,6 @@
 <?php
 require_once 'index.php';
 
-function correctsStats($monsterArray){
-    foreach ($monsterArray as $item) {
-        if (array_search('Water', $monsterArray [$strict = FALSE])){
-        echo 'Epic gamer moment';
-        }
-    }
-}
-
 function checkEmpty($testArray){
     $testArray = [1, 3 , 4];
     if (!empty($testArray)){
